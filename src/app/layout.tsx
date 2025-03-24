c4b2480b20c9fe01d6389d7b451e6fb3affcf963
Masozee <nurojilukmansyah@gmail.com>
2025-03-25 05:41:22 +0700

    add speed insight and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -37,6 +39,8 @@ export default function RootLayout({
       </head>
       <body className={`${inter.className} antialiased min-h-screen`}>
         {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
